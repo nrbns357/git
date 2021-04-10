0e16eac1f777fff6621f41f98f9e302623ad229a
nrbns357 <gusgh357@gmail.com>
2021-04-10 21:46:15 +0900

    변수 추가

--- 현호 프로그램/현호.c
+++ 현호 프로그램/현호.c
@@ -2,14 +2,14 @@
 int main_gnt() 
 
 {
-    
+    int b;
     char a[30] = "가나다라마바사아자차카타파하";
     printf("쓰고싶은 한글을 골라서 써주세요.");
     while (1)
     {
         
-    scanf_s("%s",&a );
-    printf("%s", *a);
+    scanf_s("%s",&b);
+    printf("%s", a[b]);
     if (a == 0)
         break;
     }
